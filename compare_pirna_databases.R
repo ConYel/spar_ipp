359b562a7118ba5183c7102b4914965dcc42e780
conyel <yeles.konstantinos@gmail.com>
2019-12-11 16:00:00 +0000

    manual biocpar

--- compare_pirna_databases.R
+++ compare_pirna_databases.R
@@ -56,10 +56,9 @@ pirna_cl_db <- piRNA_dbs_files[3] %>%
   keepStandardChromosomes(pruning.mode="coarse")
 # check all databases to find overlaps
 pirna_cl_db %>% join_overlap_intersect_directed(pirnadb_cl)
-overlaps_3db <- dashr_db %>% 
+#overlaps_3db <- dashr_db %>% 
   join_overlap_intersect_directed(pirnadb, minoverlap = 15) %>% 
   join_overlap_intersect_directed(pirbase, minoverlap = 15)
-
 
 # create a union of all dbs ------
 dashr_db_piRNA <- dashr_db %>% filter(dashr_type == "piRNA") %>% select(-dashr_type)
@@ -142,13 +141,18 @@ pirnadb_red %>%
                                  max=max)) %>% 
   arrange(as.character(seqnames)) %>% tail(20)
 
-pirnadb_red_short <- pirnadb_red %>% 
+pirnadb_short <- pirnadb_red %>% 
   keepStandardChromosomes(pruning.mode="coarse") %>% 
   as_tibble() %>% 
   filter(width < 40)
 # concat them and reduce
-pirna_DB_union <- c(as_granges(dashr_db_piRNA_short), as_granges(pirbase_short), as_granges(pirnadb_red_short)) %>% 
-  reduce_ranges_directed() %>%  keepStandardChromosomes(pruning.mode="coarse")
+pirna_DB_union <- c(as_granges(dashr_db_piRNA_short), as_granges(pirbase_short), as_granges(pirnadb_short)) %>% 
+  reduce_ranges_directed() %>%  
+  keepStandardChromosomes(pruning.mode="coarse") %>% 
+  as_tibble() %>%  
+  mutate(rnaID = 1:length(.$strand) %>% str_c("RNA", .)) %>% 
+  as_granges()
+
 pirna_DB_union %>% as_tibble() %>% 
   group_by(seqnames) %>% 
   summarise_at(vars(width) ,list(min = min, Q1=~quantile(., probs = 0.25),
@@ -166,8 +170,84 @@ p_DB_U <- pirna_DB_union %>%
 library(TxDb.Hsapiens.UCSC.hg38.knownGene);library(org.Hs.eg.db)
 txd <- TxDb.Hsapiens.UCSC.hg38.knownGene
 library(bumphunter)
-genes <- annotateTranscripts(TxDb.Hsapiens.UCSC.hg38.knownGene,annotation="org.Hs.eg.db")
+genes <- annotateTranscripts(TxDb.Hsapiens.UCSC.hg38.knownGene,annotation="org.Hs.eg.db") %>% keepStandardChromosomes(pruning.mode="coarse")
 #annotated_peaks <- matchGenes(p_DB_U, genes, type = "any", promoterDist = 2500, skipExons = FALSE, verbose = TRUE) %>% as_tibble()
+#p_DB_U_chr1 <- pirna_DB_union %>% filter(seqnames == "chr1")
+p_DB_U_chr <- map(seqlevels(pirna_DB_union), ~ pirna_DB_union  %>% 
+                     filter(seqnames == .x))
 
-p_DB_U_chr11 <- p_DB_U %>% filter(seqnames == "chr11")
 annotated_peaks_chr11 <- matchGenes(p_DB_U_chr11, genes, type = "any", promoterDist = 2500, skipExons = FALSE, verbose = TRUE) %>% as_tibble()
+chrY <- 
+test1 <- list(p_DB_U_chr[[25]][1:100],  p_DB_U_chr[[23]])
+
+library(BiocParallel)
+SerialParam()
+MulticoreParam()
+mt_param <- MulticoreParam(workers = 6)
+fun <- function(v){
+  message("working")
+  matchGenes(v, genes, type = "any", promoterDist = 2500, skipExons = FALSE, verbose = TRUE) %>% as_tibble()
+}
+
+test_genes_1 <- bplapply(test1, fun, BPPARAM = mt_param)  
+test_genes_1 <- test_genes_1 %>% bind_rows()
+
+test1 <- bind_ranges(test1)
+
+test_genes_1 <- test_genes_1 %>% 
+  bind_rows() %>% 
+  bind_cols(as_tibble(test1)) %>% 
+  dplyr::select(name:subjectHits,rnaID)
+seq(1,340000,by= 25000)
+
+p_DB_U_chr_1_1 <- p_DB_U_chr[[1]][1:25000]
+p_DB_U_chr_1_2 <- p_DB_U_chr[[1]][25001:50000]
+p_DB_U_chr_1_3 <- p_DB_U_chr[[1]][50001:75000]
+p_DB_U_chr_1_4 <- p_DB_U_chr[[1]][75001:100000]
+p_DB_U_chr_1_5 <- p_DB_U_chr[[1]][100001:125000]
+p_DB_U_chr_1_6 <- p_DB_U_chr[[1]][125001:150000]
+p_DB_U_chr_1_7 <- p_DB_U_chr[[1]][150001:175000]
+p_DB_U_chr_1_8 <- p_DB_U_chr[[1]][175001:200000]
+p_DB_U_chr_1_9 <- p_DB_U_chr[[1]][200001:225000]
+p_DB_U_chr_1_10 <- p_DB_U_chr[[1]][225001:250000]
+p_DB_U_chr_1_11 <- p_DB_U_chr[[1]][250001:275000]
+p_DB_U_chr_1_12 <- p_DB_U_chr[[1]][275001:300000]
+p_DB_U_chr_1_13 <- p_DB_U_chr[[1]][300001:325000]
+p_DB_U_chr_1_14 <- p_DB_U_chr[[1]][325001:338735]
+
+test3_chr1 <- list(p_DB_U_chr_1_1, p_DB_U_chr_1_2, 
+                   p_DB_U_chr_1_3, p_DB_U_chr_1_4,
+                   p_DB_U_chr_1_5, p_DB_U_chr_1_6,
+                   p_DB_U_chr_1_7, p_DB_U_chr_1_8,
+                   p_DB_U_chr_1_9, p_DB_U_chr_1_10,
+                   p_DB_U_chr_1_11, p_DB_U_chr_1_12,
+                   p_DB_U_chr_1_13, p_DB_U_chr_1_14)
+
+test1 <- list(p_DB_U_chr_Y_1, p_DB_U_chr_Y_2, 
+              p_DB_U_chr_Y_3, p_DB_U_chr_Y_4,
+              p_DB_U_chr_Y_5, p_DB_U_chr_Y_6, 
+              p_DB_U_chr_Y_7, p_DB_U_chr_Y_8, 
+              p_DB_U_chr_Y_9, p_DB_U_chr_Y_10,
+              p_DB_U_chr_Y_11, p_DB_U_chr_Y_12)
+
+test2 <- list(p_DB_U_chr_21_1, p_DB_U_chr_21_2, 
+              p_DB_U_chr_21_3, p_DB_U_chr_21_4,
+              p_DB_U_chr_21_5, p_DB_U_chr_21_6)
+test_genes_2 <- bplapply(test2, fun, BPPARAM = mt_param)
+
+
+test2 <- bind_ranges(test2)
+test_genes_2 <- test_genes_2 %>%
+  bind_rows() %>% 
+  bind_cols(as_tibble(test2)) %>% 
+  dplyr::select(name:subjectHits,rnaID)
+
+chr_21_Y <- bind_rows(test_genes_1,test_genes_2)
+
+p_DB_U %>% 
+  as_tibble %>% 
+  inner_join(chr_21_Y, by = "rnaID") %>% 
+  write_tsv("all_DB_genes_chrY_21.txt")
+
+mt_param <- MulticoreParam(workers = 14)
+test_genes_3_chr1 <- bplapply(test3_chr1, fun, BPPARAM = mt_param) 
